54063f205d63d2bf7f4fbc5e41a96a8fa4edffe8
Udayan Gupta <udayangupta97@gmail.com>
2021-12-17 10:42:08 +0530

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React App to search GitHub profiles and see profile details.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'> 1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Developed By :{' '}
+        <a className='text-white' href='https://github.com/UdynGP'>
+          Udayan Gupta
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+};
