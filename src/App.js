50b69ca31657251cf4ab18e50bdc42799f9237fd
Udayan Gupta <udayangupta97@gmail.com>
2021-12-18 21:44:25 +0530

    UI setup complete

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext';
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
-          </Routes>
-        </main>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
---
+++ src/context/github/GithubReducer.jsx
@@ -0,0 +1,25 @@
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case 'CLEAR_SEARCH':
+      return {
+        ...state,
+        users: [],
+        loading: false,
+      };
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default githubReducer;
