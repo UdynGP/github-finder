e1d859746b3f7a94b29f28def3c767bc8ad25689
Udayan Gupta <udayangupta97@gmail.com>
2021-12-19 22:56:26 +0530

    Refactored components. Final commit

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,22 +1,26 @@
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
 import AlertContext from '../../context/alert/AlertContext';
+import { searchUsers } from '../../context/github/GithubActions';
 
 function UserSearch() {
   const [text, setText] = useState('');
 
-  const { users, searchUsers, clearSearch } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (text === '') {
       setAlert('Search field cant be blank!', 'error');
     } else {
-      searchUsers(text);
+      dispatch({ type: 'SET_LOADING' });
+      const users = await searchUsers(text);
+      dispatch({ type: 'GET_USERS', payload: users });
+
       setText('');
     }
   };
@@ -46,7 +50,10 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={clearSearch} className='btn btn-ghost btn-lg'>
+          <button
+            onClick={() => dispatch({ type: 'CLEAR_SEARCH' })}
+            className='btn btn-ghost btn-lg'
+          >
             Clear
           </button>
         </div>
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,21 +1,27 @@
 import { FaCode, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
 import { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import Spinner from '../components/layout/Spinner';
 import { useParams } from 'react-router-dom';
-import GithubContext from '../context/github/GithubContext';
+import Spinner from '../components/layout/Spinner';
 import RepoList from '../components/repos/RepoList';
+import GithubContext from '../context/github/GithubContext';
+import { getUserAndRepos } from '../context/github/GithubActions';
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-  }, []);
+    dispatch({ type: 'SET_LOADING' });
+
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
